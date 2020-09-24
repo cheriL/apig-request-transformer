0e401fb8c9d493bfe0f45563e1c979823062c038
zhangzhq04 <zhangzhq04@inspur.com>
2020-09-24 11:43:04 +0800

    remove unused code

--- kong/plugins/apig-request-transformer/access.lua
+++ kong/plugins/apig-request-transformer/access.lua
@@ -1,10 +1,8 @@
-local cjson = require "cjson.safe"
 local path_params_mgr = require "kong.plugins.apig-request-transformer.path_params"
 
 local _M = {}
 --define
 local CONTENT_TYPE = "Content-Type"
-local CONTENT_LENGTH = "Content-Length"
 local HOST = "host"
 
 local JSON = "json"
@@ -22,26 +20,7 @@ local find = string.find
 local upper = string.upper
 local lower = string.lower
 local gsub = string.gsub
-local pairs = pairs
-local insert = table.insert
-local noop = function() end
 
-local function append_value(current_value, value)
-    local current_value_type = type(current_value)
-  
-    if current_value_type  == "table" then
-      insert(current_value, value)
-      return current_value
-    end
-  
-    if current_value_type == "string"  or
-       current_value_type == "boolean" or
-       current_value_type == "number" then
-      return { current_value, value }
-    end
-  
-    return { value }
-end
 
 local function get_content_type(content_type)
     if content_type == nil then
@@ -52,10 +31,10 @@ local function get_content_type(content_type)
 
     if find(content_type, "application/json", nil, true) then
         return JSON
-    end 
+    end
     if find(content_type, "application/x-www-form-urlencoded", nil, true) then
         return FORM
-    end 
+    end
     if find(content_type, "multipart/form-data", nil, true) then
         return MULTIPART
     end
@@ -74,47 +53,12 @@ local function iter(config_array)
         if current_pair == nil then
             return nil
         end
-        local pos1, key1, 
+        local pos1, key1,
             pos2, key2 = current_pair:match("^([^:]+):(.+);([^:]+):(.+)$")
         return pos1, key1, pos2, key2
     end, config_array
 end
 
-local function set_json_value(table, key, value)
-    if not key or not value or key == '' then
-        return table
-    end
-
-    local json_table = table
-    if not json_table then 
-        json_table = {}
-    end
-
-    local json_path = key
-    
-    local pos = string.find(json_path, "%.")
-    if not pos then
-        if json_table[json_path] ~= nil then
-            json_table[json_path] = append_value(json_table[json_path], value)
-        else
-            json_table[json_path] = value
-        end
-    elseif pos == 1 then
-        json_path = string.sub(json_path, pos + 1, #json_path)
-        json_table = set_json_value(json_table, json_path, value)
-    elseif pos == #json_path then
-        json_path = string.sub(json_path, 1, #json_path - 1)
-        json_table = set_json_value(json_table, json_path, value)
-    else
-        local node_name = string.sub(json_path, 1, pos - 1)
-        json_path = string.sub(json_path, pos + 1, #json_path)
-        if not json_table[node_name] then
-            json_table[node_name] = {}
-        end
-        json_table[node_name] = set_json_value(json_table[node_name], json_path, value)
-    end
-    return json_table
-end
 
 --[[******************************************************************
 FunctionName:	change_head_value
@@ -125,7 +69,7 @@ Parameter:
         3 key       [string]              参数的key
         4 value     [string, number, nil] 待设置的参数值
 
-Return:		
+Return:
         opt为0，返回headers表和提取的参数值
         opt为1，返回headers表
 
@@ -159,7 +103,7 @@ Parameter:
         3 key       [string]              参数的key
         4 value     [string, number, nil] 待设置的参数值
 
-Return:	
+Return:
         opt为0，返回querys表和提取的参数值
         opt为1，返回querys表
 
@@ -182,32 +126,6 @@ local function change_query_value(opt, querys, key, value)
     end
 end
 
---[[******************************************************************
-FunctionName:	set_body_value
-Purpose:		修改请求body
-Parameter:
-        1 key       [string]              参数的key
-        2 value     [string, number, nil] 待设置的参数值
-        3 body      [table]               body参数表 
-        4 content_type  [string]          Content-Type属性 
-
-Return:	
-        返回body参数表
-
-Remark: 
-********************************************************************--]]
-local function set_body_value(key, value, body, content_type)
-  --[[
-  if content_type == JSON then
-  elseif content_type == FORM then
-    if type(body) == "table" then
-      body[key] = value
-    end
-  end
-
-  return body
-  --]]
-end
 
 local function transform_param(ori_table, trans_table, conf)
     local headers = ori_table.headers
@@ -219,7 +137,7 @@ local function transform_param(ori_table, trans_table, conf)
     if backend_content_type ~= nil then
         headers = change_head_value(1, headers, CONTENT_TYPE, conf.backendContentType)
     end
-    
+
     headers = change_head_value(1, headers, HOST, nil)
 
     local replace  = 0 < #conf.replace
@@ -254,10 +172,10 @@ local function transform_param(ori_table, trans_table, conf)
                 --提取参数值
                 local pos1 = lower(req_param_pos)
                 if pos1 == QUERY then
-                    querys, value = change_query_value(0, querys, req_param)                    
-                    if not query_changed then query_changed = true end        
+                    querys, value = change_query_value(0, querys, req_param)
+                    if not query_changed then query_changed = true end
                 elseif pos1 == HEAD then
-                    headers, value = change_head_value(0, headers, req_param)           
+                    headers, value = change_head_value(0, headers, req_param)
                 elseif pos1 == PATH then
                     if type(path_params) == "table" and next(path_params) then
                         value = path_params[req_param]
@@ -272,17 +190,17 @@ local function transform_param(ori_table, trans_table, conf)
                     headers[backend_param] = value
                 elseif pos2 == QUERY then
                     querys[backend_param] = value
-                    if not query_changed then query_changed = true end 
+                    if not query_changed then query_changed = true end
                 elseif pos2 == PATH then
                     backend_path = gsub(backend_path, '{'.. backend_param .. '}', value)
                     if not path_changed then path_changed = true end
                 end
-                
+
                 break --跳出while true
             end
         end
     end
-    
+
     trans_table.headers = headers
     if query_changed then
         trans_table.querys = querys
@@ -308,5 +226,5 @@ function _M.execute(ori_table, conf)
 
     return trans_table
 end
-  
+
 return _M
